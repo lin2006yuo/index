eaaa9cf15cd41847a0890eb76bcb249a3d1f88ad
linxueyu <lin2006yuo@gmail.com>
2020-08-28 22:46:34 +0800

    mo

--- src/components/framer/index.tsx
+++ src/components/framer/index.tsx
@@ -12,8 +12,8 @@ import {
 } from "framer"
 import { useEffect } from "react"
 const Framer = () => {
-  const x = useMotionValue(0)
-  const scale = useTransform(x, [0, 200], [1, 0.5])
+  // const x = useMotionValue(0)
+  // const scale = useTransform(x, [0, 200], [1, 0.5])
 
   return null
 }
